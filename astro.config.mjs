191df189e49daecfcbd074e7921cb54d4409e8ca
Dan Dervich <dandervich@hotmail.com>
2025-04-16 11:44:55 -0400

    final commit

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from 'astro/config';
+// Import /static for a static site
+import vercelStatic from '@astrojs/vercel/static';
+
+export default defineConfig({
+    // Must be 'static' or 'hybrid'
+    output: 'static',
+    adapter: vercelStatic({
+        webAnalytics: {
+            enabled: true
+        }
+    }),
+});
